517ecdece69932da2e30c29d9f1a3f12e867b3fb
usa0w0 <kingkazuma1130@gmail.com>
2022-11-21 10:17:29 +0900

    予約者DB

--- 00_property.js
+++ 00_property.js
@@ -5,3 +5,6 @@ let loginManagerName = "";
 // システム設定スプレッドシート
 const SettingSpreadSheet = SpreadsheetApp.openById("1r8rFRh-kApFbPuNxALtvhHG9ekGAjKkUlGUMImW1aFo");
 const Memberslist = SettingSpreadSheet.getSheetByName("管理者権限").getDataRange().getValues().flat();
+
+// DBスプレッドシート
+const DBSpreadSheet = SpreadsheetApp.openById("1rs_eCD--OZPGFeo_Nd5tXKgoXDF0GgmFXQF6xENi3UE");
