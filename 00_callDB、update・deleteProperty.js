60f7c7321dad56ac3abddea1a9f65dd7b2e735f6
usa0w0 <kingkazuma1130@gmail.com>
2022-11-22 12:01:47 +0900

    予約者情報DBの追加とそれに伴う新規シート作成などに関する改変

--- 00_callDB、update・deleteProperty.js
+++ 00_callDB、update・deleteProperty.js
@@ -1,26 +1,48 @@
 /*
 # ワークショップ管理
-dic workshopDB = {uuid: {name: , date: , start_time: , end_time: , subscription: , capacity: , question: [MODULE, ... ]}, ... }
+dic workshopDB = {uuid: WORKSHOP, ... }
+dic WORKSHOP = {name: , date: , start_time: , end_time: , subscription: , capacity: , question: [MODULE, ... ]}
 dic MODULE = {type: , title: , subscription: (type:見出しのみ), required: (type:見出し以外), option: (ラジオボタン or チェックボックス のみ)}
 */
 
 function callDB(){
+  // スクリプトプロパティからDB取得
   const workshopDB = JSON.parse(PropertiesService.getScriptProperties().getProperty("講座情報"));
   return workshopDB
 }
 
 function updateProperty(WORKSHOP, unique_id){
+  // DBを参照
   var workshopDB = callDB();
+  // uuidで指定して更新
   workshopDB[unique_id] = WORKSHOP;
+  // json形式でスクリプトプロパティを更新
+  const jsonData = JSON.stringify(workshopDB);
+  PropertiesService.getScriptProperties().setProperty("講座情報", jsonData);
+
+  // 既存のものか確認
+  if (!Object.keys(IdList).includes(unique_id)){
+    // ID対応表に追加
+    
+    // 予約者シートをテンプレートからコピー
+    TemplateSheet.copyTo(DBSpreadSheet).setName(WORKSHOP.name);
+  }
+  return 0;
+}
+
+function deleteProperty(unique_id){
+  // DBを参照
+  var workshopDB = callDB();
+  // uuidで指定して削除
+  delete workshopDB[unique_id]
+  // json形式でスクリプトプロパティを更新
   const jsonData = JSON.stringify(workshopDB);
   PropertiesService.getScriptProperties().setProperty("講座情報", jsonData);
   return 0;
 }
 
-function deleteProperty(unique_id){
-  var workshopDB = callDB();
-  delete workshopDB[unique_id]
-  const jsonData = JSON.stringify(workshopDB);
-  PropertiesService.getScriptProperties().setProperty("講座情報", jsonData);
-  return 0;
+function saveRequest(unique_id, ANSWER){
+  // 予約申請を保存させたい
+  return 0
 }
+
