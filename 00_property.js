e5650ca125804e5e53f3012ca6816ff5d63c56f8
usa0w0 <kingkazuma1130@gmail.com>
2022-12-06 16:20:28 +0900

    管理者をスプレッドシートからUI操作で設定できるように

--- 00_property.js
+++ 00_property.js
@@ -3,8 +3,11 @@ const mailAdress = Session.getActiveUser().getUserLoginId();
 let loginManagerName = "";
 
 // システム設定スプレッドシート
-const Memberslist = ["c5622040@aoyama.jp", "篠原和真", "管理者"];
+const managerData = JSON.parse(PropertiesService.getScriptProperties().getProperty("管理ステータス"));
 
 // DBスプレッドシート
 const DBSpreadSheet = SpreadsheetApp.openById("1sj3AwNJmg0M_FL_6YcdJmF-ZtuqdcGavRqZPkIIOvIk");
 const TemplateSheet = DBSpreadSheet.getSheetByName("template");
+
+// サーバーサイド時刻
+const ServerTime = (new Date()).getTime();
---
+++ 04_controlPanel.js
@@ -0,0 +1,35 @@
+function showDialog(){
+  const html = HtmlService.createTemplateFromFile('04_managerPanel')
+               .evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
+               .setWidth(1000)
+               .setHeight(1000)
+               .setTitle('管理者設定パネル');
+  return DBSpreadSheet.show(html);
+}
+
+function setDialog(){
+  // 集計SSへ追加する機能メニュー
+  const menu = [
+    {name: "管理パネル", functionName: "showDialog"}
+  ];
+
+  DBSpreadSheet.addMenu('管理者設定', menu);
+}
+
+function setMenu(){
+  ScriptApp.newTrigger('setDialog').forSpreadsheet(DBSpreadSheet).onOpen().create();
+}
+
+function saveStatus(id, data){
+  console.log(id, data)
+  // ステータスデータを更新
+  managerData[id] = data
+  // JSON形式にしてスクリプトプロパティを更新
+  const jsonData = JSON.stringify(managerData);
+  PropertiesService.getScriptProperties().setProperty("管理ステータス", jsonData);
+  return 1
+}
+
+function getStatus(){
+  return managerData
+}
