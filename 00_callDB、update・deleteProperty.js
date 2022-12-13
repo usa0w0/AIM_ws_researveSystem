678f4cf0c7441ee2e4945a9ca91592a0e4247b58
Kazuma.S <kingkazuma1130@gmail.com>
2022-12-13 23:41:25 +0900

    ファイルを分割

--- 00_callDB、update・deleteProperty.js
+++ 00_callDB、update・deleteProperty.js
@@ -159,37 +159,3 @@ function deleteProperty(unique_id){
 
   return 0;
 }
-
-function saveRequest(unique_id, ANSWER){
-  const WORKSHOP = callDB()[unique_id]
-  const workshop_name = WORKSHOP.name
-
-  // 予約者シートを取得
-  const DBSheet = DBSpreadSheet.getSheetByName(workshop_name + "_" + unique_id);
-  
-  console.log(ANSWER)
-
-  // 回答を保存
-  let request = new Array(new Date(), mailAdress).concat(ANSWER.map(answer => answer.value));
-  DBSheet.getRange(DBSheet.getLastRow()+1, 1, 1, request.length).setValues([request]);
-
-  return 0
-}
-
-function cancelRequest(unique_id){
-  const WORKSHOP = callDB()[unique_id]
-  const workshop_name = WORKSHOP.name
-
-  // 予約者シートを取得
-  const DBSheet = DBSpreadSheet.getSheetByName(workshop_name + "_" + unique_id);
-  const data = DBSheet.getDataRange().getValues();
-
-  for (let i=0; i<DBSheet.getLastRow(); i++){
-    console.log(data[i])
-    if (data[i][1] == mailAdress){
-      DBSheet.deleteRow(i+1)
-    }
-  }
-
-  return 0
-}
